3f34446c2765e239d66c6cae71d687864f18e8f0
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-06-26 23:18:06 +0600

    js file add and add layout button

---
+++ app.js
@@ -0,0 +1,11 @@
+const button1 = () => {
+    document.getElementById('container').innerHTML = "Hello layout1";
+}
+
+const button2 = () => {
+    document.getElementById('container').innerHTML = "Hello layout2";
+}
+
+const button3 = () => {
+    document.getElementById('container').innerHTML = "Hello layout3";
+}
