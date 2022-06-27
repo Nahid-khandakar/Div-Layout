a5b6a363bfb23dbf66a4fe0882e64a72e9c2f597
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-06-27 15:37:50 +0600

    fix problem

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 const button1 = () => {
     document.getElementById('button1').style.backgroundColor = "rgba(96, 96, 96, 1)";
+    document.getElementById('button1').style.color = "rgba(224, 224, 224, 1)";
 
     document.getElementById('button2').style.backgroundColor = "rgba(224, 224, 224, 1)";
+    document.getElementById('button2').style.color = "rgba(96, 96, 96, 1)";
     document.getElementById('button3').style.backgroundColor = "rgba(224, 224, 224, 1)";
+    document.getElementById('button3').style.color = "rgba(96, 96, 96, 1)";
 
     document.getElementById("container").className = 'layout1';
 }
 
 const button2 = () => {
     document.getElementById('button2').style.backgroundColor = "rgba(96, 96, 96, 1)";
+    document.getElementById('button2').style.color = "rgba(224, 224, 224, 1)";
 
     document.getElementById('button1').style.backgroundColor = "rgba(224, 224, 224, 1)";
+    document.getElementById('button1').style.color = "rgba(96, 96, 96, 1)";
     document.getElementById('button3').style.backgroundColor = "rgba(224, 224, 224, 1)";
+    document.getElementById('button3').style.color = "rgba(96, 96, 96, 1)";
 
     document.getElementById("container").className = "layout2";
 }
 
 const button3 = () => {
     document.getElementById('button3').style.backgroundColor = "rgba(96, 96, 96, 1)";
+    document.getElementById('button3').style.color = "rgba(224, 224, 224, 1)";
 
     document.getElementById('button1').style.backgroundColor = "rgba(224, 224, 224, 1)";
+    document.getElementById('button1').style.color = "rgba(96, 96, 96, 1)";
     document.getElementById('button2').style.backgroundColor = "rgba(224, 224, 224, 1)";
+    document.getElementById('button2').style.color = "rgba(96, 96, 96, 1)";
 
     document.getElementById('container').className = 'layout3'
 }
